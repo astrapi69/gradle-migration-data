d0717d3b0a293848f8b7ceae2c37ee013d878521
Asterios Raptis <asteri.raptis@gmail.com>
2024-06-10 18:34:05 +0200

    fixed broken build on github

--- src/test/java/io/github/astrapi69/gradle/migration/toml/ProjectTomlStructureInfo.java
+++ src/test/java/io/github/astrapi69/gradle/migration/toml/ProjectTomlStructureInfo.java
@@ -1,19 +1,20 @@
 package io.github.astrapi69.gradle.migration.toml;
 
+import java.io.File;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
-import java.io.File;
-
 @Data
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ProjectTomlStructureInfo {
-    File gradleDirectory;
-    String sourceProjectDirNamePrefix;
-    String targetProjectDirNamePrefix;
-    String sourceProjectName;
-    String targetProjectName;
+public class ProjectTomlStructureInfo
+{
+	File gradleDirectory;
+	String sourceProjectDirNamePrefix;
+	String targetProjectDirNamePrefix;
+	String sourceProjectName;
+	String targetProjectName;
 }
