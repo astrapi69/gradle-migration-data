cf0fb082de0702af976b52a1e2c236dac4076837
Asterios Raptis <asteri.raptis@gmail.com>
2024-09-06 16:01:29 +0200

    change of target project name

--- src/test/java/io/github/astrapi69/gradle/migration/toml/MigrateToTomlVersionsTest.java
+++ src/test/java/io/github/astrapi69/gradle/migration/toml/MigrateToTomlVersionsTest.java
@@ -136,7 +136,7 @@ public class MigrateToTomlVersionsTest
 
 		gradleDirectory = getGradleDirectory();
 
-		targetProjectName = "silly-collection";
+		targetProjectName = "comparator-extensions";
 		sourceProjectName = DependenciesInfo.JAVA_LIBRARY_TEMPLATE_NAME;
 		sourceGithubUser = "astrapi69";
 		// targetGithubUser = "lightblueseas";
